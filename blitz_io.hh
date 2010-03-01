7e53dae45092e4d0e5c50b330e8cb3156d80ac88
dmeliza <dmeliza@uchicago.edu>
2010-03-01 09:08:37 -0600

    blitz_io.hh: added some mirror methods for std::string, other fixes
    Ignore-this: 99b873a8559e9a0df730fc3dafa6771b
    
    darcs-hash:20100301150837-21d25-cd2ce581686f7ab268a633887e1573e5aeffe059.gz

--- blitz_io.hh
+++ blitz_io.hh
@@ -28,6 +28,11 @@ int read_bin(const char *filename, blitz::Array<T,2> &out) {
 	return count;
 }
 
+template<typename T> inline
+int read_bin(const std::string &filename, blitz::Array<T,2> &out) {
+	return read_bin(filename.c_str(), out);
+}
+
 
 template<typename T, int N>
 int write_bin(const char *filename, const blitz::Array<T,N> &data) {
@@ -45,6 +50,12 @@ int write_bin(const char *filename, const blitz::Array<T,N> &data) {
 	fclose(fp);
 	return ct;
 }
+
+
+template<typename T, int N> inline
+int write_bin(const std::string &filename, const blitz::Array<T,N> &data) {
+	return write_bin(filename.c_str(), data);
+}
 	
 
 template<typename T>
@@ -55,6 +66,9 @@ struct timeseries {
 
 	timeseries(blitz::Array<T,1> &_samples, unsigned int _samplerate) :
 		samples(_samples), samplerate(_samplerate) {}
+
+	timeseries(unsigned int _nsamples, unsigned int _samplerate) :
+		samples(blitz::Array<T,1>(_nsamples)), samplerate(_samplerate) {}
 
 	timeseries(const char *filename) {
 		sf_count_t count, nframes;
@@ -71,10 +85,19 @@ struct timeseries {
 		samples.resize(count);
 	}
 
-	int write_pcm(const char *filename, int format) {
+	timeseries(const std::string &filename) {
+		timeseries(filename.c_str());
+	}
+
+	int write_pcm(const char *filename, int format = SF_FORMAT_WAV | SF_FORMAT_PCM_16, 
+		      bool norm=true) {
 		SndfileHandle fp = SndfileHandle(filename, SFM_WRITE, format, 1, samplerate);
+		if (fp.error())
+			throw std::runtime_error("Error opening file: " + std::string(fp.strError()));
+		fp.command(SFC_SET_NORM_DOUBLE,NULL,norm ? SF_TRUE : SF_FALSE);
+		fp.command(SFC_SET_NORM_FLOAT,NULL,norm ? SF_TRUE : SF_FALSE);
 		const T *pData = samples.data();
-		return fp.write(pData, samples.size());
+		return fp.writef(pData, samples.size());
 	}
 };
 
